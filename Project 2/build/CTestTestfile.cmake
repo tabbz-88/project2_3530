0ff9b23e11af81ccae69254c34e939d57c68838b
Bao Tran <baott0711@gmail.com>
2025-10-14 22:37:38 -0400

    Delete Project 2 directory

--- Project 2/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/zactr/OneDrive - University of Florida/COP Programs/COP3530/Project 2
-# Build directory: C:/Users/zactr/OneDrive - University of Florida/COP Programs/COP3530/Project 2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
