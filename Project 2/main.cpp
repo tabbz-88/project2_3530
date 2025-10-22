5841934c8e09a7cc6744253e0b19b60e5c14bf09
Elijah De Guzman <63167131+Cloud-Multiplayer@users.noreply.github.com>
2025-10-22 18:38:39 -0400

    Name loop and space issue fixed(?)

--- Project 2/main.cpp
+++ Project 2/main.cpp
@@ -9,27 +9,29 @@ int main() {
     cout << "Loading...\n";
 
     {
+
         Taxon taxon;
         CommonName common;
 
         if (!taxon.load("NameUsage.tsv")) return 1;
         if (!common.load("VernacularName.tsv")) return 1;
 
-        string name1, name2;
-        cout << "\nEnter first species (common or scientific name): ";
-        getline(cin, name1);
-        cout << "Enter second species (common or scientific name): ";
-        getline(cin, name2);
 
-        string id1 = common.getCommonID(name1);
-        if (id1.empty()) id1 = taxon.getScientificID(name1);
+        string name1, name2, id1, id2;
+        while (id1.empty() || id2.empty()) {
+            cout << "\nEnter first species (common or scientific name): ";
+            getline(cin, name1);
+            cout << "Enter second species (common or scientific name): ";
+            getline(cin, name2);
 
-        string id2 = common.getCommonID(name2);
-        if (id2.empty()) id2 = taxon.getScientificID(name2);
+            id1 = common.getCommonID(name1);
+            if (id1.empty()) id1 = taxon.getScientificID(name1);
 
-        if (id1.empty() || id2.empty()) {
-            cout << "One or both species not found in dataset.\n";
-            return 0;
+            id2 = common.getCommonID(name2);
+            if (id2.empty()) id2 = taxon.getScientificID(name2);
+
+            if (id1.empty()) cout << name1 << " is not found in dataset.\n";
+            if (id2.empty()) cout << name2 << " is not found in dataset.\n";
         }
 
         double relatedness = Relation::compute(taxon, id1, id2);
